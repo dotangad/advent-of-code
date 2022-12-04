071a921db211978f50585a7ed62277bd3835070e
Angad Singh <mail@angad.dev>
2022-12-04 10:52:26 +0530

    (2022) day 4

---
+++ 2022/4.ts
@@ -0,0 +1,63 @@
+import * as _ from "lodash";
+import { chain as ch } from "lodash";
+import { readInput } from "./utils";
+
+type Section = number[];
+type InputType = Section[][];
+const input: InputType =
+  ch(readInput(process.argv[2]))
+    .split("\n")
+    .filter(x => !!x)
+    .map(x =>
+      x.split(',')
+        .map(y =>
+          y.split('-')
+            .map(z => parseInt(z.trim()))
+        ))
+    .value();
+
+function one(input: InputType) {
+  const sectionContained = (s1: Section, s2: Section) => {
+    const [s1b, s1e] = s1;
+    const [s2b, s2e] = s2;
+
+    if (s2b >= s1b && s2e <= s1e) return true
+    if (s1b >= s2b && s1e <= s2e) return true
+
+    return false;
+  }
+
+  return ch(input)
+    .map(([s1, s2]) => sectionContained(s1, s2))
+    .countBy(_.identity)
+    .value()['true'];
+}
+
+function two(input: InputType) {
+  const between = (x: number, y: number, a: number) => {
+    const low = Math.min(x, y), high = Math.max(x, y);
+
+    if(a >= low && a <= high) return true;
+
+    return false;
+  }
+
+  const sectionOverlap = (s1: Section, s2: Section) => {
+    const [s1b, s1e] = s1;
+    const [s2b, s2e] = s2;
+
+    if(between(s1b, s1e, s2b) || between(s1b, s1e, s2e)) return true;
+    if(between(s2b, s2e, s1b) || between(s2b, s2e, s1e)) return true;
+
+    return false;
+  }
+
+
+  return ch(input)
+    .map(([s1, s2]) => sectionOverlap(s1, s2))
+    .countBy(_.identity)
+    .value()['true'];
+}
+
+console.log(`Part 1: ${one(input)}`);
+console.log(`Part 2: ${two(input)}`);
