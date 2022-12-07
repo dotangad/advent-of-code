58821d871fd63cf8adf2803994d5e50fa3b6e108
Angad Singh <mail@angad.dev>
2022-12-07 12:31:20 +0530

    (2022-ex) run doctests

---
+++ 2022-ex/test/advent_of_code_test.exs
@@ -0,0 +1,30 @@
+defmodule AdventOfCodeTest do
+  use ExUnit.Case, async: true
+  doctest AdventOfCode.Day01
+  doctest AdventOfCode.Day02
+  doctest AdventOfCode.Day03
+  doctest AdventOfCode.Day04
+  doctest AdventOfCode.Day05
+  doctest AdventOfCode.Day06
+  doctest AdventOfCode.Day07
+  doctest AdventOfCode.Day08
+  doctest AdventOfCode.Day09
+  doctest AdventOfCode.Day10
+  doctest AdventOfCode.Day11
+  doctest AdventOfCode.Day12
+  doctest AdventOfCode.Day13
+  doctest AdventOfCode.Day14
+  doctest AdventOfCode.Day15
+  doctest AdventOfCode.Day16
+  doctest AdventOfCode.Day17
+  doctest AdventOfCode.Day18
+  doctest AdventOfCode.Day19
+  doctest AdventOfCode.Day20
+  doctest AdventOfCode.Day21
+  doctest AdventOfCode.Day22
+  doctest AdventOfCode.Day23
+  doctest AdventOfCode.Day24
+  doctest AdventOfCode.Day25
+
+  import AdventOfCode
+end
